b0a7601bda5139c4ab9cb51daa697ab88515c4c3
zlf <820023459@qq.com>
2021-03-06 20:15:06 +0800

    1.3.1
    合并关系图到项目里

--- Diabetes_Data_Visualization/views.py
+++ Diabetes_Data_Visualization/views.py
@@ -18,12 +18,21 @@ def overall(request):
     return render(request, 'overall.html', ctx)
 
 
+def pienest(request):
+    ctx = {}
+    ctx['num_diabetes_complication'] = data_process.barData()
+    ctx['scatterData'] = data_process.scatterData()
+    ctx['bloodPressureData'] = data_process.bloodPressureData()
+    ctx['diseaseRelationshipData'] = data_process.relationshipData()
+    return render(request, 'pie-nest.html', ctx)
+
+
 def scatterData(request):
     if request.POST:
         left, right = request.POST['left'], request.POST['right']
         left, right = int(left), int(right)
         ret = data_process.scatterData(left, right)
-        #print(ret)
+        # print(ret)
         ret = {'data': ret}
         ret = JsonResponse(ret)
         return ret
@@ -45,6 +54,7 @@ def getAbnormalAttr(request):
         ret = {'data': ret}
         ret = JsonResponse(ret)
         return ret
+
 
 def getRadarInfo(request):
     if request.POST:
